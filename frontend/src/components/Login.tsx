3731681daa7cdc910cfe58ed74e3879efd070f4d
Luis Uzcategui <alfonsouzcategui1811@gmail.com>
2026-05-12 21:45:47 -0500

    login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -116,19 +116,19 @@ const Login: React.FC = () => {
 
                 <form onSubmit={handleSubmit}>
                   <div className='mb-3'>
-                    <label htmlFor='identifier' className='form-label'>
+                    {/* <label htmlFor='identifier' className='form-label'>
                       Email o Usuario
-                    </label>
-                    <input type='text' className='form-control' id='identifier' name='identifier' placeholder='admin@diur.com' value={formData.identifier} onChange={handleChange} required />
+                    </label> */}
+                    <input type='text' className='form-control' id='identifier' name='identifier' placeholder='Email o Usuario' value={formData.identifier} onChange={handleChange} required />
                   </div>
 
                   <div className='mb-3'>
-                    <label htmlFor='password' className='form-label'>
+                    {/* <label htmlFor='password' className='form-label'>
                       Contraseña
-                    </label>
-                    <div className='d-flex align-items-center' style={{position: 'relative'}}>
-                      <input type={showPassword ? 'text' : 'password'} className='form-control' id='password' name='password' placeholder='••••••••' value={formData.password} onChange={handleChange} required />
-                      <svg style={{position: 'absolute', right: '.5rem'}} onClick={() => setShowPassword(!showPassword)} xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#000000'>
+                    </label> */}
+                    <div className='d-flex align-items-center' style={{ position: 'relative' }}>
+                      <input type={showPassword ? 'text' : 'password'} className='form-control' id='password' name='password' placeholder='Contraseña' value={formData.password} onChange={handleChange} required />
+                      <svg style={{ position: 'absolute', right: '.5rem' }} onClick={() => setShowPassword(!showPassword)} xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#999999'>
                         {showPassword ? (
                           <path d='m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z' />
                         ) : (
@@ -150,10 +150,6 @@ const Login: React.FC = () => {
                     </button>
                   </div>
                 </form>
-
-                <div className='small text-muted'>
-                  Acceso de prueba: <strong>admin@diur.com</strong> / <strong>admin123</strong>
-                </div>
               </div>
             </div>
           </div>
